27aaca5f4d8088e90ed88b25edfeaf7daccd4d1a
otaki444 <xm.ms.430x@gmail.com>
2020-03-16 22:33:35 +0900

    [update]5-9

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,29 @@
+class BooksController < ApplicationController
+	def new
+		@book = Book.new
+	end
+	def create
+		@book = Book.new(book_params)
+		@book.user_id = current_user.user_id
+		if @book.save
+			redirect_to book_path
+		else
+			render :new
+		end
+	end
+	def index
+		@books = Book.all
+	end
+	def show
+		@book = Book.find(params[:id])
+	end
+	def destroy
+		@book = Book.find(params[:id])
+		@book.destroy
+		redirect_to book_path
+	end
+	private
+	def book_params
+		params.require(:book).permit(:title, :body)
+	end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         has_many :book, dependent: :destroy
+         validates :name,    length: { in: 2..20 }
+         attachment :profile_image
 end
